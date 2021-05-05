3acc5a14dd4f4ac2ff090f8ab035fb41f306cb30
Juan Cruz Parise <63892409+juancruzparise@users.noreply.github.com>
2021-05-05 16:13:12 -0300

    Add files via upload

---
+++ js/game-main2.js
@@ -0,0 +1,68 @@
+// Variables que se usan en el codigo
+var pics = [0,1,2,3,4,5,6,7,8,9,10,11];
+var numClick=0;
+var position1;
+var audiowin = document.getElementById("audiowin");
+
+// Llamo  a las funciones 
+disorder();
+reset();
+uncheckAll();
+
+// Funcion para desordenar las fotos
+function disorder() {
+	pics.sort(function() {return Math.random() - 0.5});
+	console.log(pics);
+}
+function uncheckAll() {
+	for (var i=0;i<=11;i++) {
+		document.getElementById("img_"+i).style.border = "solid 2px white";
+	}
+}
+//Función para hacer click
+function selectImage(site) {
+	numClick = numClick + 1;
+
+	if (numClick == 1) {
+		position1 = site;
+		uncheckAll();
+		document.getElementById("img_"+site).style.border = "solid 2px #5C239E";
+
+	}
+
+	if (numClick==2) {
+		var position2 = site;
+		var contenido = pics[position1];
+		pics[position1] = pics[position2];
+		pics[position2] = contenido;
+		numClick=0;
+		uncheckAll();
+		reset();
+		var fin = youWin();
+		if (fin == true) {
+				audiowin.play();
+				setTimeout(function(){
+                    alert("You win , Please click restart to play again "); 
+                }, 200);
+		}
+	}
+}
+// Funcion para saber si ganaste
+function youWin() {
+	var fin = true;
+	for (var i=0;i<=11;i++) {
+		if (pics[i] != i) {
+			fin = false;
+		}
+	}
+	return fin;
+	
+}
+
+function reset() {
+	for (var site=0;site<=11;site++) {
+		var imagen = pics[site];
+		document.getElementById("img_"+site).src = "image/images2/image_"+imagen+".jpg";
+	}
+}
+
---
+++ js/switch.js
@@ -0,0 +1,30 @@
+// Button dark mode
+function darkMode() {
+    var element = document.getElementById("bodygame")
+    element.classList.toggle("dark-mode");
+      }
+// MODAL
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
